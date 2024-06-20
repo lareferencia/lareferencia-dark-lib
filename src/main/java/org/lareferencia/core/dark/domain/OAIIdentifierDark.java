d66a79fbd54999e623cdce3150b5d24f6302e844
Lautaro Matas <lmatas@gmail.com>
2024-06-20 18:33:45 +0200

    Domain classes, repositories and services for dark collector storage

---
+++ src/main/java/org/lareferencia/core/dark/domain/OAIIdentifierDark.java
@@ -0,0 +1,58 @@
+/*
+ *   Copyright (c) 2013-2022. LA Referencia / Red CLARA and others
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Affero General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Affero General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Affero General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *   This file is part of LA Referencia software platform LRHarvester v4.x
+ *   For any further information please contact Lautaro Matas <lmatas@gmail.com>
+ */
+
+package org.lareferencia.core.dark.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+/**
+ */
+@Entity
+@Getter
+@Setter
+public class OAIIdentifierDark {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	protected Long id = null;
+
+	@Column(nullable = false)
+	private String oaiIdentifier;
+
+	@Column(nullable = false)
+	private String darkIdentifier;
+
+	@Column(nullable = false)
+	protected Boolean metadata = false;
+
+	public OAIIdentifierDark(String oaiIdentifier, String darkIdentifier) {
+		this.oaiIdentifier = oaiIdentifier;
+		this.darkIdentifier = darkIdentifier;
+	}
+
+}
---
+++ src/main/java/org/lareferencia/core/dark/services/DarkIdentifierStoreService.java
@@ -0,0 +1,34 @@
+package org.lareferencia.core.dark.services;
+
+import org.lareferencia.core.dark.domain.OAIIdentifierDark;
+import org.lareferencia.core.dark.repositories.OAIIdentifierDarkRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class DarkIdentifierStoreService {
+
+    private final OAIIdentifierDarkRepository repository;
+
+    @Autowired
+    public DarkIdentifierStoreService(OAIIdentifierDarkRepository repository) {
+        this.repository = repository;
+    }
+
+    public OAIIdentifierDark addDarkIdentifier(String oaiIdentifier, String darkIdentifier) {
+        OAIIdentifierDark newPair = new OAIIdentifierDark(oaiIdentifier, darkIdentifier);
+        return repository.save(newPair);
+    }
+
+    public Optional<String> getDarkIdentifierByOAIIdentifier(String oaiIdentifier) {
+        return repository.findByOaiIdentifier(oaiIdentifier)
+                .map(OAIIdentifierDark::getDarkIdentifier);
+    }
+
+    public Optional<String> getOaiIdentifierByDark(String darkIdentifier) {
+        return repository.findByDarkIdentifier(darkIdentifier)
+                .map(OAIIdentifierDark::getOaiIdentifier);
+    }
+}
